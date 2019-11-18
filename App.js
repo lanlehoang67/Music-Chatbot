8cb54f956d1662eb45000cbfa31e57d24ffe7a15
Lan <lanlehoang10@gmail.com>
2019-11-18 13:30:39 +0700

    add

--- App.js
+++ App.js
@@ -1,91 +1,29 @@
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
-import { Dialogflow_V2 } from 'react-native-dialogflow';
-import { dialogflowConfig } from './env';
-import axios from 'axios';
-const BOT_USER = {
-  _id: 2,
-  name: 'FAQ Bot',
-  avatar: 'https://i.imgur.com/7k12EPD.png'
-};
+// import the different screens
+import Loading from './components/Loading'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Main from './components/Main'
 
-class App extends Component {
-  state = {
-    messages: [
-      {
-        _id: 1,
-        text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
-        createdAt: new Date(),
-        user: BOT_USER
-      }
-    ]
-  };
-
-  componentDidMount() {
-    Dialogflow_V2.setConfiguration(
-      dialogflowConfig.client_email,
-      dialogflowConfig.private_key,
-      Dialogflow_V2.LANG_ENGLISH_US,
-      dialogflowConfig.project_id
-    );
-  }
-
-  handleGoogleResponse(result) {
-    console.log(result.queryResult)
-    let text = ""
-    if(result.queryResult.fulfillmentMessages[0].simpleResponses== undefined){
-      text = result.queryResult.fulfillmentMessages[0].text.text[0];
-
-    }
-    else {
-      text = JSON.parse(JSON.stringify(result.queryResult.fulfillmentMessages[0].simpleResponses)).simpleResponses[0].textToSpeech ;
-    }
+// create our app's navigation stack
+const App = createStackNavigator(
+ {
+   Login: {
+     screen: Login,
     
-    this.sendBotResponse(text);
-    
-  }
+   },
+   SignUp: {
+     screen: SignUp,
+   },
+   Main: {
+     screen: Main,
+   
+   }
+ },
+)
 
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages)
-    }));
-
-    let message = messages[0].text;
-    Dialogflow_V2.requestQuery(
-      message,
-      result => this.handleGoogleResponse(result),
-      error => console.log(error)
-    );
-  }
-
-  sendBotResponse(text) {
-    let msg = {
-      _id: this.state.messages.length + 1,
-      text,
-      createdAt: new Date(),
-      user: BOT_USER
-    };
-
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, [msg])
-    }));
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: '#fff' }}>
-        <GiftedChat
-          messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1
-          }}
-        />
-      </View>
-    );
-  }
-}
-
-export default App;
+export default createAppContainer(App)
